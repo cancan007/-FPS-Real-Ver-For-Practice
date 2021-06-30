c467d5e3795b6026209420abc69980ebf5bcbc0d
cancan007 <81799825+cancan007@users.noreply.github.com>
2021-07-01 02:02:29 +0900

    Learnt how to set cursol and camera player movement

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -2,17 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// 参考URL: https://youtu.be/hFyKH5jh6UQ
+
 public class FPSController : MonoBehaviour
 {
+    // 移動用の変数を作成
+    float x, z;
+
+    // スピード調整用の変数を作成
+    float speed = 0.1f;
+
+    public GameObject cam;  // カメラ
+    Quaternion cameraRot, characterRot;  // カメラとプレイヤーの角度(Quaternion は角度を用いる時に使う)
+    float Xsensitivity = 3f, Ysensitivity = 3f;  //カメラとプレイヤーの操作感度
+
+    bool cursorLock = true;     //true:非表示,  false:表示
+
+    float minX = -90f, maxX = 90f;   //角度に制限を儲けるための数値
+
     // Start is called before the first frame update
-    void Start()
+    void Start()   // void: 戻り値を返さない関数を定義するときに使う
     {
+        cameraRot = cam.transform.localRotation;  // 初めに作成しておきたいので、Start内に
+        characterRot = transform.localRotation;  
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xRot = Input.GetAxis("Mouse X") * Ysensitivity;　// Input 入力キーに対する操作
+        float yRot = Input.GetAxis("Mouse Y") * Xsensitivity; // xを軸に取った縦方向の回転
+
+        cameraRot *= Quaternion.Euler(-yRot, 0, 0);  //(x,y,z) それぞれの角度を設定 (-にしたのは、今回逆だから)
+                     // yに設定しないのは、カメラ方向に基づいて移動するときに、水平方向のみ移動可能にするため
+                 // 水平方向の回転は親オブジェクトであるplayerに対応させて、カメラはⅹを軸にした縦方向の回転縛りにしたい    
+
+        characterRot *= Quaternion.Euler(0, xRot, 0);  // 軸を元に回転するので、x座標にyRot. y座標にxRot
+                                                       // Playerはyを軸にした水平方向の回転縛りにしたい
+
+        cameraRot = ClampRotation(cameraRot);
+
+        cam.transform.localRotation = cameraRot; // カメラ角度の更新を反映
+        transform.localRotation = characterRot;  // プレイヤー角度更新を反映
+
+        UpdateCursorLock();
+    }
+
+    private void FixedUpdate()  //0.02秒ごとに行う
+    {
+        x = 0;
+        z = 0;
+
+        x = Input.GetAxisRaw("Horizontal") * speed;
+        z = Input.GetAxisRaw("Vertical") * speed;
+
+        //transform.position += new Vector3(x, 0, z);  // プレイヤーの位置を更新
+
+        transform.position += cam.transform.forward * z + cam.transform.right * x;  // カメラの向いている方向に基づいて進むように設定
+                  // forward: z軸,    right: x軸
+    }
+
+    public void UpdateCursorLock()   //カーソルを表示するかしないか
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))   //Escapeボタンを押したとき
+        {
+            cursorLock = false;
+        }
+        else if (Input.GetMouseButton(0))   //マウスボタンの左を押したとき
+        {
+            cursorLock = true;
+        }
+
+        if (cursorLock)  //trueのとき
+        {
+            Cursor.lockState = CursorLockMode.Locked;    // 非表示にする
+        }
+
+        else if(!cursorLock)  //falseのとき
+        {
+            Cursor.lockState = CursorLockMode.None;   // 表示にする
+        }
+    }
+
+    public Quaternion ClampRotation(Quaternion q)   //この関数ではQuaternionを戻り値として返す
+    {
+        //q = x,y,z,w (x,y,zはベクトル(量と向き)  :wはスカラー(座標とは無関係の量:回転する))
+
+        // 単位ベクトルにする(角度を求めるため)
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1f;
+
+        float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;   //角度をラジアンで表示
+
+        angleX = Mathf.Clamp(angleX, minX, maxX);  // Clamp:　値の下限と上限を設定
+
+        q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
+
+        return q;
     }
 }
